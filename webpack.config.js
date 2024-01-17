b4e4ac472245e4173271ff32adba6026bfdf5226
YonugJones <peterckerfoot@gmail.com>
2024-01-17 10:30:09 -0600

    Installed babel and updated webpack.config with babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,4 +10,20 @@ module.exports = {
   resolve: {
     extensions: ['.js'],
   },
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
+    ]
+  }
 };
